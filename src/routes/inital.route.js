7b5627cecc1f6bc7821aafca83e0b4768f179484
jaffasunny <jaffer.sunny125@gmail.com>
2024-01-15 14:08:02 +0500

    modified

--- src/routes/inital.route.js
+++ src/routes/inital.route.js
@@ -5,7 +5,9 @@ const router = Router();
 router.route("/").get((req, res) => {
 	return res
 		.status(200)
-		.send("<h1>Welcome to intial route for Backend Mern Ecommerce 2024</h1>");
+		.send(
+			"<h1>Welcome to intial route for Backend Mern Ecommerce 2024....</h1>"
+		);
 });
 
 export default router;
